662ef6422f896b6b3d99f265806ab8e7da2fb54a
MrSinaMaleki <sina67631@gmail.com>
2024-08-12 23:14:57 +0330

    db setup (models.py)

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name="title")
+    author = models.CharField(max_length=200, verbose_name="author")
+
+    isbn = models.CharField(max_length=13, verbose_name="Shabak Serial")
+
+    date_published = models.DateTimeField(auto_now_add=True, db_index=True)
+    date_edited = models.DateTimeField(null=True, auto_now=True)
